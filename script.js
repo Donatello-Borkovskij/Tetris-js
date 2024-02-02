eb916cbb93eb39845f770fb6b1626caa3dbcc866
Donat <donatborkovskij@gmail.com>
2024-02-02 21:35:32 +0200

    canvas fix

--- script.js
+++ script.js
@@ -20,21 +20,3 @@ function Draw() {
   ctx.fillStyle = "red";
   ctx.fillRect(50, 50, 50, 50);
 }
-
-function SetupCanvas() {
-  canvas = document.getElementById("Tetris");
-  ctx = canvas.getContext("2d");
-  canvas.width = 936;
-  canvas.height = 956;
-
-  // Double the size of elements to fit the screen
-  ctx.scale(2, 2);
-
-  // Draw Canvas background
-  ctx.fillStyle = "white";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Draw gameboard rectangle
-  ctx.strokeStyle = "black";
-  ctx.strokeRect(8, 8, 280, 462);
-}
