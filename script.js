dc88928f3fa25b0e964e83a69e759218c8ef4707
Donat <donatborkovskij@gmail.com>
2024-02-03 18:23:43 +0200

    added grid

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 const canvas = document.getElementById("Tetris");
 const ctx = canvas.getContext("2d");
-const fieldSize = [9, 19]; //x, y
-const startXY = [4, 0];
+const width = 400;
+const height = 800;
+
+let score = 0;
 
 document.addEventListener("DOMContentLoaded", Draw);
 
 function Draw() {
-  canvas.width = 400;
-  canvas.height = 800;
+  canvas.width = width;
+  canvas.height = height;
 
-  ctx.fillStyle = "white";
+  ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // Draw a red rectangle at position (50, 50) with width 50 and height 50
   ctx.fillStyle = "red";
-  ctx.fillRect(50, 50, 50, 50);
+  ctx.fillRect(40, 40, 40, 40);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(40, 80, 40, 40);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(40, 120, 40, 40);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(80, 120, 40, 40);
+
+  ctx.lineWidth = 2;
+  ctx.strokeStyle = "grey";
+
+  for (let i = 0; i < height; i += width / 10) {
+    // Draw a vertical line
+    if (i < width) {
+      ctx.beginPath();
+      ctx.moveTo(i, 0); // Starting point (x, y)
+      ctx.lineTo(i, height); // Ending point (x, y)
+      ctx.stroke(); // Render the line
+    }
+    // Draw a horizontal line
+    ctx.beginPath();
+    ctx.moveTo(0, i);
+    ctx.lineTo(width, i);
+    ctx.stroke();
+  }
 }
